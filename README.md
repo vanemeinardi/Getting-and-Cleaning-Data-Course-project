0dc826713dcae02694f150a3927fa44f72442a5c
vanemeinardi <vanemeinardi@gmail.com>
2016-04-06 20:20:00 -0700

    edit readme and add new files codebook.m, run_analysis.R and tidy.txt

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Getting-and-Cleaning-Data-Course-project
 Final project of the course Gettind and Cleaning Data
+
+The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis.
+##PROJECT DESCRIPTION
+The R script, run_analysis.R, does the following on the UCI HAR Dataset download from 
+###https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+*1.Merges the training and the test sets to create one data set.
+*2.Extracts only the measurements on the mean and standard deviation 
+for each measurement.
+*3.Uses descriptive activity names to name the activities in the data set
+*4.Appropriately labels the data set with descriptive variable names.
+*5.From the data set in step 4, creates a second, independent tidy data set with 
+the average of each variable for each activity and each subject.
+
+## PROJECT SUMMARY
+The following is a sumary of the activities performed to achieve the steps 1-5 *Download the dataset if it does not already exist in the working directory
+*Set the working directory
+*Load the  raw data from file UCI HAR Dataset and assign column names
+*Create the trainingData table and the testData table by merging the train data set and the test data set respectively
+*Merge the two data sets.
+*Create a logical vector to keep the columns with mean and standart deviation.
+* Use the logical vector to obtain a new table with the columns desired.
+*Give  descriptive names to the activities using the activity_labels table.
+*Converts the activity and subject columns into factors.
+*Creates a tidy dataset that consists of the average (mean) value of each variable for each subject and activity pair.
+*The end result is shown in the file tidy.txt.
+
+## WHAT YOU FIND IN THIS REPOSITORY
+
+*CodeBook.md: information about raw and tidy data set and elaboration made to transform them
+*README.md: this file
+*run_analysis.R: R script to transform raw data set in a tidy one
+tidy.txt
---
+++ run_analysis.R
@@ -0,0 +1,108 @@
+#This script will do the following steps on the UCI HAR Dataset,
+#1.Merges the training and the test sets to create one data set.
+#2.Extracts only the measurements on the mean and standard deviation 
+#for each measurement.
+#3.#Uses descriptive activity names to name the activities in the data set
+#4.Appropriately labels the data set with descriptive variable names.
+#5.From the data set in step 4, creates a second, independent tidy data set with 
+#the average of each variable for each activity and each subject.
+
+###################################################################
+# 1. Merge the training and the test sets to create one data set.
+###################################################################
+
+#First of all we will set the working directory
+setwd("C:/Users/vanesameinardi/Desktop/DataScience Specialization/UCI HAR Dataset")
+
+#Then load raw data from file UCI HAR Dataset and assign column names. 
+features <- read.table("./features.txt", colClasses = c("character"))
+activityType <- read.table("./activity_labels.txt", col.names = c("ActivityId", "Activity"))
+
+
+#Load raw data from the train dataset on UCI HAR Dataset and assign column names. 
+xTrain <- read.table("./train/x_train.txt", col.names = features[,2])
+yTrain <- read.table("./train/y_train.txt", col.names= "ActivityId")
+subjectTrain <- read.table("./train/subject_train.txt", col.names = "SubjectId" )
+
+# Create the trainingData table by merging yTrain, subjectTrain, and xTrain by column.
+trainingData = cbind(yTrain,subjectTrain,xTrain)
+
+#Load raw data from test dataset and assign column names.
+xTest <- read.table("./test/x_test.txt", col.names = features[,2])
+yTest <- read.table("./test/y_test.txt", col.names= "ActivityId")
+subjectTest <- read.table("./test/subject_test.txt", col.names = "SubjectId" )
+
+# Create the testData table by merging the xTest, yTest and subjectTest data by column.
+testData = cbind(yTest,subjectTest,xTest)
+
+#Merge the trainingData and the testData to creat one table by row.
+data <- rbind(trainingData, testData)
+
+#############################################################################
+#2.Extracts only the measurements on the mean and standard deviation for each 
+#measurement.
+#############################################################################
+
+#Create a logical vector that contains true for the columns that have the word M[m]ean,std, 
+#SubjectId or ActivityId.
+#in their name.
+logicalvector=(grepl("[Mm]ean|[Ss]td|SubjectId|ActivityId", names(data), ignore.case = FALSE))
+
+#Subset the data table based when the logical vector is TRUE to obtain the column desired
+data <- data[logicalvector == TRUE]
+
+#3.Uses descriptive activity names to name the activities in the data set. 
+#Merge the activityType table with the data table by the column "activityId" to give 
+# descriptive names to the activities
+
+data <- merge(data, activityType, by = "ActivityId", all.x = TRUE)
+
+
+#######################################################################
+# 4. Appropriately label the data set with descriptive activity names. 
+#######################################################################
+
+# Create a vector for the column names from the data, which will be used
+# to improving the variable names of the columns.
+colNames  = colnames(data) 
+
+# Improving the variable names
+colNames <- gsub("\\.", "", colNames)
+colNames <-  gsub("^(t)","Time",colNames)
+colNames<- gsub("mean","Mean",colNames)
+colNames<- gsub("std","StDev",colNames)
+#colNames <- gsub("mad","MedAbsDev",colNames)
+#colNames <- gsub("max","Max",colNames)
+#colNames <-  gsub("min","Min",colNames)
+#colNames <- gsub("sma","SigMagArea",colNames)
+#colNames <- gsub("energy","Energy",colNames)
+#colNames <- gsub("iqr","InterqRange",colNames)
+#colNames <- gsub("entropy","Entropy",colNames)
+#colNames <- gsub("arCoeff","AutoRegCoeff",colNames)
+#colNames <- gsub("correlation","Correlation",colNames)
+colNames <-  gsub("^(f)","Freq",colNames)
+#colNames <- gsub("kewness","Kewness",colNames)
+#colNames <-  gsub("kurtosis","Kurtosis",colNames)
+#colNames <- gsub("bands","Bands",colNames)
+colNames <- gsub("angle","Angle",colNames)
+colNames <- gsub("gravity","Gravity",colNames)
+ 
+#Update colnames
+colnames(data) = colNames
+
+#################################################################################
+#5.From the data set in step 4, creates a second, independent tidy data set with 
+#the average of each variable for each activity and each subject.
+#################################################################################
+#Remove column ActivityId with select. Load package dplyr
+library(dplyr)
+data <- select(data , -ActivityId)
+#Convert in factor Activity and SubjectId to use in melt function
+data$Activity <- factor(data$Activity)
+data$SubjectId <- as.factor(data$SubjectId)
+library(reshape2)
+datamelt <- melt(data, id = c("SubjectId", "Activity"))
+datamean <- dcast(datamelt, SubjectId + Activity ~ variable, mean)
+
+#Finally write a table with the tidy data.
+write.table(datamean, "tidy.txt", row.names = FALSE, quote = FALSE)
